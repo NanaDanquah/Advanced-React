f116183c55b088a50e601dae460b8c4b5fc78953
Gabriel <danquahbadugabriel@gmail.com>
2025-10-15 18:23:39 +0000

    created a custom hook

--- React Hooks and Custom Hooks/custom-hooks/src/App.jsx
+++ React Hooks and Custom Hooks/custom-hooks/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [day, setDay] = useState('Monday');
+
+  const prevDay = usePrevious(day);
+
+  const getNextDay = ()=> {
+    switch(day){
+      case "Monday":
+        setDay("Tuesday");
+        break;
+
+      case "Tuesday":
+        setDay("Wednesday");
+        break;
+      
+      case "Wednesday":
+        setDay("Thursday");
+        break;
+
+      case "Thursday":
+        setDay("Friday");
+        break;
+
+      case "Friday":
+        setDay("Monday")
+        break;
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>
+        Today is: {day}<br />
+        {
+          prevDay && (
+            <span>Previous work day was: {prevDay} </span>
+          )
+        }
+      </h1>
+      <button onClick={getNextDay}>
+        Get next day
+      </button>
+    </div>
+    
   )
 }
 
-export default App
+function usePrevious(val){
+  const ref = useRef();
+  useEffect(()=>{
+    ref.current = val;
+  }, [val]);
+  return ref.current;
+}
